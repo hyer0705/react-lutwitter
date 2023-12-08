f073225cdf52e65e9356ab66f7e8564cd7d5c39d
hyer0705 <hyer1205@gmail.com>
2023-12-08 18:05:49 +0900

    Fix img file check condition

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -145,7 +145,7 @@ export default function PostTweetForm() {
             {...register("img", {
               validate: {
                 maxFileSize: (v) => {
-                  if (v) {
+                  if (v && v.length > 0) {
                     return (
                       v[0].size < 1 * 1024 * 1024 ||
                       "1MB 이하의 이미지 파일만 가능합니다."
